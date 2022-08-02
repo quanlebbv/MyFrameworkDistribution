8d45483b430bdbc0a0c5557b1a654aa4e21a474e
Kiet Truong <quan.leminh@hotmail.com>
2022-08-02 14:54:11 +0700

    updated package.

--- cocoapod.sh
+++ cocoapod.sh
@@ -19,5 +19,6 @@ git tag -m "update podspec" $version
 git push --tags
 
 sha=$(git rev-parse HEAD)
+ref=refs/heads/$version
 
-curl -X POST https://api.github.com/repos/quanlebbv/MyFrameworkDistribution/git/refs -H "Accept: application/vnd.github+json" -H "Authorization: token $1" -d '{"ref":"refs/heads/$version", "sha":$sha}'
+curl -X POST https://api.github.com/repos/quanlebbv/MyFrameworkDistribution/git/refs -H "Accept: application/vnd.github+json" -H "Authorization: token $1" -d '{"ref":"'$ref'", "sha":"'$sha'"}'
